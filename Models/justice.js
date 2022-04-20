94c89b3006a1c84693341af77b6024340e84b7f5
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-04-20 14:12:12 -0400

    Models completed.

---
+++ Models/justice.js
@@ -0,0 +1,13 @@
+const justice = [
+    {
+        name: 'Batman',
+        ability: 'Deep Voice',
+        img: 'http://cdn.shopify.com/s/files/1/0549/7769/1857/products/Batman_475pin.png?v=1628557427',
+
+    },{
+        name: 'Superman',
+        ability: 'Laser Beam',
+        img: 'https://www.elcbrands.com/media/superman1.1.jpg'
+    }]
+
+    module.exports = justice
